ae38dc3d126813af9575344c9de6c43fdba59a5c
TheOneDV <dvofficial.cam@gmail.com>
2022-08-23 01:47:57 +0700

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author ""
+description ''
+version '1.0'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/drawtext.js',
+    'html/drawtext.css'
+}
